eeaa1ed0c0f0ef7348a99d882d303347e93bd005
130017 郝阡喻 <130017@cycoa.com.tw>
2024-04-09 13:58:35 +0800

    add Data/ApplicationDbContext

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace TeaTimeDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
